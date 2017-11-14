5d5e39046c02174a5db295f1a5dace564d8b0466
yaojie <limyaojie93@gmail.com>
2017-11-14 11:28:42 +0800

    update calendar to not use timezone

--- src/main/java/app/ClassController.java
+++ src/main/java/app/ClassController.java
@@ -3,7 +3,15 @@ package app;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import models.CohortClass;
@@ -30,15 +39,61 @@ public class ClassController {
      */
     @RequestMapping(value="/classes/{klass}", method=RequestMethod.GET)
     public String getClasses(@PathVariable String klass){
+    	String result = getTimetable(klass).toString();
+    	return result;
+    }
+    
+    @RequestMapping(value="/classes/{klass}/this_week", method=RequestMethod.GET)
+    public String getClassesThisWeek(@PathVariable String klass){
     	String result = null;
+    	
+    	JsonArray timetable = getTimetable(klass);
+    	JsonArray coursesInWeek = new JsonArray();
+    	
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeZone(TimeZone.getTimeZone("GMT+0"));
+		// get the current week to check
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.clear(Calendar.MINUTE);
+		cal.clear(Calendar.SECOND);
+		cal.clear(Calendar.MILLISECOND);
+		cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
+		
+		Calendar nextWeek = (Calendar) cal.clone();
+		nextWeek.add(Calendar.WEEK_OF_YEAR, 1);
+
+    	for (JsonElement element: timetable){
+    		JsonObject obj = element.getAsJsonObject();
+    		
+    		String start = obj.get("start").getAsString();
+    		String end = obj.get("end").getAsString();
+    		
+    		// java8 date parsing
+			Date startDate = Date.from(Instant.parse(start+'Z'));
+			Date endDate = Date.from(Instant.parse(end+'Z'));
+			
+			if (startDate.after(cal.getTime()) && endDate.before(nextWeek.getTime())){
+				// if startDate occurs after this week and before
+				coursesInWeek.add(obj);
+			}
+    	}
+
+		result = coursesInWeek.toString();
+		
+    	return result;
+    }
+    
+    private JsonArray getTimetable(String klass){
     	HttpURLConnection connection = null;
+    	JsonArray result = null;
+    	
     	try {
 			URL timetableURL = new URL("http://sutd-timetable.herokuapp.com/group_sections/?" + klass);
 			connection = (HttpURLConnection) timetableURL.openConnection();
 			connection.connect();
 			if (connection.getResponseCode() == HttpURLConnection.HTTP_OK){
 				JsonElement root = new JsonParser().parse(new InputStreamReader(connection.getInputStream()));
-				result = root.getAsJsonObject().get("events").getAsJsonArray().toString();
+				result = root.getAsJsonObject().get("events").getAsJsonArray();
 			} else {
 				throw new Exception("Server responded with a response code of: " + connection.getResponseCode());
 			}
@@ -47,7 +102,7 @@ public class ClassController {
 		} finally {
 			connection.disconnect();
 		}
+    	
     	return result;
     }
-
 }
