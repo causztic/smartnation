1fc2125851ad3241eae9dd389ee445e482ac1636
yaojie <limyaojie93@gmail.com>
2017-12-03 16:24:44 +0800

    update readme with endpoints

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ https://projectlombok.org/download
 ## Install packages and migrate database
 ```mvn install```
 ```mvn package && java -jar target/smartnation-0.1.0.jar``` 
+
+## Endpoints
+```/classes```
+```/classes/{klass}```
+```/classes/{klass}/this_week```
+```/area/{type}```, where type is **food** or **meeting**
+```/stats/{area_id}?[from,to,part]```, where from and to are timestamps, and part is **hour**, **day** or **minute** 
--- src/main/resources/db/migration/afterMigrate.sql
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -17,5 +17,33 @@ BEGIN
       ('MeetingArea', 1.340761, 103.962566, 'Library (2nd floor)', 'library', 'https://s3-ap-southeast-1.amazonaws.com/floating-forest/library_level2.jpg', 'https://s3-ap-southeast-1.amazonaws.com/floating-forest/library_level2.jpg'),
       ('MeetingArea', 1.340761, 103.962566, 'Library (3rd floor)', 'library', 'https://s3-ap-southeast-1.amazonaws.com/floating-forest/library_level3.jpg', 'https://s3-ap-southeast-1.amazonaws.com/floating-forest/library_level3.jpg');
   END IF;
+
+  IF NOT EXISTS ( SELECT 1 from statistic ) THEN
+   /* set some demo data */
+    INSERT INTO statistic ( data_date, count, area_id ) VALUES
+      (TIMESTAMP '2017-01-01 09:30:00', 10, 1),
+      (TIMESTAMP '2017-01-01 10:00:00', 12, 1),      
+      (TIMESTAMP '2017-01-01 10:30:00', 15, 1),
+      (TIMESTAMP '2017-01-01 11:00:00', 10, 1), 
+      (TIMESTAMP '2017-01-01 11:30:00', 15, 1),
+      (TIMESTAMP '2017-01-01 12:00:00', 16, 1), 
+      (TIMESTAMP '2017-01-01 12:30:00', 10, 1),
+      (TIMESTAMP '2017-01-01 13:00:00', 14, 1), 
+      (TIMESTAMP '2017-01-01 13:30:00', 20, 1),
+      (TIMESTAMP '2017-01-01 14:00:00', 12, 1), 
+      (TIMESTAMP '2017-01-01 14:30:00', 14, 1),
+      (TIMESTAMP '2017-01-01 15:00:00', 4, 1), 
+      (TIMESTAMP '2017-01-01 15:30:00', 9, 1),
+      (TIMESTAMP '2017-01-01 16:00:00', 3, 1), 
+      (TIMESTAMP '2017-01-01 16:30:00', 10, 1),
+      (TIMESTAMP '2017-01-01 17:00:00', 9, 1), 
+      (TIMESTAMP '2017-01-01 17:30:00', 8, 1),
+      (TIMESTAMP '2017-01-01 18:00:00', 29, 1), 
+      (TIMESTAMP '2017-01-01 18:30:00', 8, 1),
+      (TIMESTAMP '2017-01-01 19:00:00', 10, 1), 
+      (TIMESTAMP '2017-01-01 19:30:00', 3, 1),
+      (TIMESTAMP '2017-01-01 20:00:00', 4, 1), 
+      (TIMESTAMP '2017-01-01 20:30:00', 2, 1);
+  END IF;
 END;
 $$
