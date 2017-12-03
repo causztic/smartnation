221ca23c04eb69b0cfdd2a4da89b0c3405d4df82
yaojie <limyaojie93@gmail.com>
2017-12-03 15:53:12 +0800

    refactor controller to use map

--- src/main/java/app/StatisticController.java
+++ src/main/java/app/StatisticController.java
@@ -2,6 +2,7 @@ package app;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,30 +20,27 @@ public class StatisticController {
 	private StatisticRepository repo;
 	
 	@RequestMapping(value="/stats/{area}", method=RequestMethod.GET, produces="application/json")
-	public String getStatsByArea(@PathVariable int area){
+	public String getStatsByArea(@PathVariable int area, @RequestParam Map<String,String> allRequestParams){
+		if (allRequestParams.containsKey("from") && allRequestParams.containsKey("to")){
+			if (allRequestParams.containsKey("part")) {
+				String part = allRequestParams.get("part");
+				switch (part){
+				case "hour":
+				case "minute":
+				case "day":
+					List<Statistic> stats = repo.averagePerHour(area, allRequestParams.get("to"), allRequestParams.get("from"), part);
+					return new Gson().toJson(stats, new TypeToken<ArrayList<Statistic>>() {}.getType());
+				default:
+					throw new IllegalArgumentException("Part has to be either 'hour', 'minute', or 'day'.");
+				}
+			} else {
+				List<Statistic> stats = repo.findBetweenDates(area, allRequestParams.get("to"), allRequestParams.get("from"));
+				return new Gson().toJson(stats, new TypeToken<ArrayList<Statistic>>() {}.getType());
+			}
+		}
+
 		List<Statistic> stats = repo.findByArea(area);
 		return new Gson().toJson(stats, new TypeToken<ArrayList<Statistic>>() {}.getType());
 	}
 	
-	@RequestMapping(value="/stats/{area}?from={from}&to={to}", method=RequestMethod.GET, produces="application/json")
-	public String getStatsBetweenDates(@PathVariable int area, @PathVariable String from, @PathVariable String to){
-		List<Statistic> stats = repo.findBetweenDates(area, to, from);
-		return new Gson().toJson(stats, new TypeToken<ArrayList<Statistic>>() {}.getType());
-	}
-	
-	@RequestMapping(value="/stats/{area}?from={from}&to={to}&part={part}", method=RequestMethod.GET, produces="application/json")
-	public String getAveragetatsBetweenDates(
-			@PathVariable int area, 
-			@PathVariable String from, @PathVariable String to,
-			@PathVariable String part){
-		switch (part){
-		case "hour":
-		case "minute":
-		case "day":
-			List<Statistic> stats = repo.averagePerHour(area, to, from, part);
-			return new Gson().toJson(stats, new TypeToken<ArrayList<Statistic>>() {}.getType());
-		default:
-			throw new IllegalArgumentException("Part has to be either 'hour', 'minute', or 'day'.");
-		}
-	}
 }
